f63840d4803fddf60a4d468e3253747991fa0bd1
Vlad <vlad0102550@yandex.ru>
2019-05-05 01:14:56 +0300

    SpringBoot, PostgreSQL, Hibernate

---
+++ src/main/java/com/springjpahibernateexample/SpringJpaHibernateExampleApplication.java
@@ -0,0 +1,14 @@
+package com.springjpahibernateexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories(basePackages = "com.springjpahibernateexample.repository")
+@SpringBootApplication
+public class SpringJpaHibernateExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaHibernateExampleApplication.class, args);
+	}
+}
